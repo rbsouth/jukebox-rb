8ede32371fdbbabc9d2c5a39f34d6fe63a93307a
Reid South <rbsouth@gmail.com>
2018-02-04 02:04:51 -0700

    need to learn react funtions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Header'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  //}
+  // closeJumbo(){
+  //   this.refs.jumbotron.css('display', 'none');
+  // }
+  // setName(e){
+
+  }
   render() {
     return (
-      <div className="jumbotron">
+      <div className="jumbotron" ref="jumbotron">
         <h1 className="display-4">Welcome</h1>
         <h3 className="lead">Enter your name to begin your Jukebox Experience</h3>
         <hr className="my-4"></hr>     
-        <p className="lead">
-          <input type="text" placeholder="Type Words"/><a className="btn btn-primary btn-lg" href="#" role="button">Start Listening</a>
+        <p >
+          <input type="text" placeholder="Your Name"/><a className="btn btn-primary btn-lg" href="#" role="button">Start Listening</a>
         </p>
       </div>
     );

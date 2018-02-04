37450fbae0cb124b6fa46de02590ec34994f70be
Reid South <rbsouth@gmail.com>
2018-02-03 22:58:50 -0700

    started

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome</h1>
+        <h3 className="lead">Enter your name to begin your Jukebox Experience</h3>
+        <hr className="my-4"></hr>     
+        <p className="lead">
+          <input type="text" placeholder="Type Words"/><a className="btn btn-primary btn-lg" href="#" role="button">Start Listening</a>
         </p>
       </div>
     );
